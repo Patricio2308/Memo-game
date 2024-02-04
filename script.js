02c14df7bc2b673fe9943598087a60a905b4e0f8
Patricio2308 <patricioandres00@gmail.com>
2024-02-04 15:48:11 -0300

    Changed matrix to array

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
 let area = document.getElementById('area');
 
-let max = 4
-let fila = new Array(max)
+
+let max = 16
+let pairs = max * 2
+let board = new Array(max)
+
+/* area.style.gridTemplateColumns = `repeat(${max}, 1fr)`;
+area.style.gridTemplateRows = `repeat(${max}, 1fr)`; */
 
 function crearElemento(valor){
     let element = document.createElement('div')
     element.className = 'box'
-    element.innerHTML = valor
+    let innerElement = document.createElement('p')
+    element.appendChild(innerElement)
+    innerElement.className = 'contentBox'
+    innerElement.innerHTML = valor
+    console.log(element)
     return element
 }
 
 // se arma la matriz
-for (let i = 0; i < fila.length; i++){
-    fila[i] = new Array(max)
+for (let i = 0; i < board.length; i++){
+    board[i] = new Array(max)
 }
 
 // se inicializan los valores de las cajas y se identifican las creadas en html
-for (let i=0; i < fila.length; i++){
-    for (let j=0; j < fila.length; j++){
-        fila[i][j] = 'x'
-        area.appendChild(crearElemento(fila[i][j]))
+/* for (let i=0; i < board.length; i++){
+    for (let j=0; j < board.length; j++){
+        board[i][j] = 'x'
+        area.appendChild(crearElemento(board[i][j]))
     }
+} */
+for (let i = 0; i < board.length; i++) {
+        board[i] = 'x'
+        area.appendChild(crearElemento(board[i]))
 }
 let box = document.querySelectorAll('.box');
 
 
 // se imprimen en pantalla
-function showCard(){
-    setInterval
+function showCard(card){
+    
+    card.style.display = 'flex'
+    if(card.style.color != 'red')
+        card.style.color = 'red'
+
+    let intervalo = setInterval( ()=> {
+        card.style.color = 'black'
+        clearInterval(intervalo)
+        card.style.display = 'none'
+    }, 1000)
 }
 // generando logica de seleccion
 let sel1 = '', sel2 = ''
@@ -50,7 +72,7 @@ function resetValues(){
     sel2 = ''
 }
 function compare(){
-    if(sel1.textContent == sel2.textContent && sel2.textContent != '' && sel1.textContent != ''){
+    if (sel1.textContent == sel2.textContent && sel2.textContent != '' && sel1.textContent != '' && sel1 != sel2){
         console.log('Iguales')
         sel1.textContent = ''
         sel2.textContent = ''
@@ -65,12 +87,12 @@ function compare(){
 
 box.forEach(element => {
     element.addEventListener('click', () => {
-    selectCard(element)
+    selectCard(element.firstChild)
     
 })
 });
 
-    
+
     /* for (let i=0; i < matriz.length/2; i++){
         let contador = 0
         let pos = Math.round(Math.random() *matriz.length )
