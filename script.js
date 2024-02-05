d2310c7ce025afd9345b3d7b72bc48e875cb7bd9
Patricio2308 <patricioandres00@gmail.com>
2024-02-05 01:13:05 -0300

    deshabilitando cajas acertadas

--- script.js
+++ script.js
@@ -1,94 +1,101 @@
 let area = document.getElementById('area');
 
 
-let max = 16
+let max = 8
 let pairs = max * 2
-let board = new Array(max)
+let board = new Array(pairs)
+let currentElement
 
 /* area.style.gridTemplateColumns = `repeat(${max}, 1fr)`;
 area.style.gridTemplateRows = `repeat(${max}, 1fr)`; */
 
-function crearElemento(valor){
+function crearElemento(valor,pos){
     let element = document.createElement('div')
     element.className = 'box'
     let innerElement = document.createElement('p')
     element.appendChild(innerElement)
     innerElement.className = 'contentBox'
+    innerElement.id = `posBox${pos}`
     innerElement.innerHTML = valor
-    console.log(element)
     return element
 }
 
-// se arma la matriz
-for (let i = 0; i < board.length; i++){
-    board[i] = new Array(max)
-}
 
 // se inicializan los valores de las cajas y se identifican las creadas en html
-/* for (let i=0; i < board.length; i++){
-    for (let j=0; j < board.length; j++){
-        board[i][j] = 'x'
-        area.appendChild(crearElemento(board[i][j]))
-    }
-} */
 for (let i = 0; i < board.length; i++) {
         board[i] = 'x'
-        area.appendChild(crearElemento(board[i]))
+        area.appendChild(crearElemento(board[i], i))
 }
 let box = document.querySelectorAll('.box');
+box[0].firstChild.textContent = 'a'
 
 
 // se imprimen en pantalla
 function showCard(card){
-    
+
     card.style.display = 'flex'
     if(card.style.color != 'red')
         card.style.color = 'red'
 
     let intervalo = setInterval( ()=> {
         card.style.color = 'black'
-        clearInterval(intervalo)
         card.style.display = 'none'
+        clearInterval(intervalo)
     }, 1000)
 }
-// generando logica de seleccion
-let sel1 = '', sel2 = ''
 
-function selectCard(card){
-    showCard(card)
-    if(sel1 == '' && sel2 == ''){
-        sel1 = card
-        console.log(sel1)
-        
-    } else {
-        sel2 = card
-        console.log(sel2)
-        compare()
-    }
+// generando logica de seleccion
+let sel1 = '', sel2 = '';
+
+let selectCard = (card) =>{
     
+    if (card != null){
+        
+        console.log(card.textContent)
+
+        showCard(card)
+        if(sel1 == '' && sel2 == ''){
+            sel1 = card;
+            /* console.log('first') */
+        } else {
+            sel2 = card;
+            /* console.log('second') */
+            compare(sel1, sel2);
+        }
+    }
 }
+
 function resetValues(){
-    sel1 = ''
-    sel2 = ''
+    sel1 = '';
+    sel2 = '';
 }
-function compare(){
+function compare(sel1, sel2){
+
     if (sel1.textContent == sel2.textContent && sel2.textContent != '' && sel1.textContent != '' && sel1 != sel2){
         console.log('Iguales')
-        sel1.textContent = ''
-        sel2.textContent = ''
-        resetValues()
+        
+        setInterval(() => { /* 
+        sel1.textContent = '';
+        sel2.textContent = ''; */
+        sel1.remove();
+        sel2.remove();
+    }, 1000)
+        resetValues();
+        //borrar el elemento hijo al anotar un par
+        
 
     } else if(sel1.textContent != sel2.textContent && sel2 != ''){
         console.log('Distintos')
-        resetValues()
-    }
+        resetValues();
+    } 
+
 }
 
-
 box.forEach(element => {
-    element.addEventListener('click', () => {
-    selectCard(element.firstChild)
-    
+    element.addEventListener('click', (e) => {
+        console.log(e.target.firstChild)
+        
+        selectCard(e.target.firstChild)
 })
 });
 
