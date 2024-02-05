ea111f73f02dd269fceebc5af895ab3cf603d3b3
Patricio2308 <patricioandres00@gmail.com>
2024-02-05 13:18:03 -0300

    Pares encontrados quedan fijos y visibles

--- script.js
+++ script.js
@@ -4,107 +4,112 @@ let area = document.getElementById('area');
 let cantcards = 8
 let pairs = cantcards * 2
 let board = new Array(pairs)
+let randomIndex
 
 /* area.style.gridTemplateColumns = `repeat(${max}, 1fr)`;
 area.style.gridTemplateRows = `repeat(${max}, 1fr)`; */
 
-function crearElemento(valor,pos){
+//api https://picsum.photos/id/${numId}/200/300
+
+//generando una lista de valores para las cards
+
+let list = []
+
+for (let i = 0; i < cantcards; i++) {
+
+    for (let j = 0; j < 2; j++) {
+        randomIndex = Math.round(Math.random() * cantcards)
+        list.splice(randomIndex, 0, i); //splice(ind, cant, elementos)
+    }
+
+}
+
+function crearElemento(img,pos){
     let element = document.createElement('div')
     element.className = 'box'
-    let innerElement = document.createElement('p')
+    let innerElement = document.createElement('img')
     element.appendChild(innerElement)
     innerElement.className = 'contentBox'
-    innerElement.id = `posBox${pos}`
-    innerElement.innerHTML = valor
+    innerElement.id = pos
+    innerElement.setAttribute('src', img)
     return element
 }
 
 
 // se inicializan los valores de las cajas y se identifican las creadas en html
 for (let i = 0; i < board.length; i++) {
-        board[i] = 'x'
-        area.appendChild(crearElemento(board[i], i))
+    if (`https://picsum.photos/id/${list[i] * randomIndex + 300}/200/200` != null){
+        board[i] = `https://picsum.photos/id/${list[i] * randomIndex + 300}/200/200`
+        area.appendChild(crearElemento(board[i], list[i]))}
 }
-let box = document.querySelectorAll('.box');
-box[0].firstChild.textContent = 'a'
-
-
-// se imprimen en pantalla
-function showCard(card){
-
-    card.style.display = 'flex'
-    if(card.style.color != 'red')
-        card.style.color = 'red'
-
-    let intervalo = setInterval( ()=> {
-        card.style.color = 'black'
-        card.style.display = 'none'
-        clearInterval(intervalo)
-    }, 1000)
-}
+var box = document.querySelectorAll('.box');
 
 // generando logica de seleccion
-let sel1 = '', sel2 = '';
+let sel1 = '';
+let sel2 = '';
 
-let selectCard = (card) =>{
-    
-    if (card != null){
-        
-        console.log(card.textContent)
+// se imprimen en pantalla
+let showCard = (card) => {
+     
+    card.style.visibility = 'visible'
 
-        showCard(card)
-        if(sel1 == '' && sel2 == ''){
-            sel1 = card;
-            /* console.log('first') */
-        } else {
-            sel2 = card;
-            /* console.log('second') */
-            compare(sel1, sel2);
-        }
-    }
 }
+
+
 
 function resetValues(){
     sel1 = '';
     sel2 = '';
 }
-function compare(sel1, sel2){
 
-    if (sel1.textContent == sel2.textContent && sel2.textContent != '' && sel1.textContent != '' && sel1 != sel2){
+function compare(sel1, sel2){
+    
+    if (sel1.id == sel2.id && sel2.id != '' && sel1.id != '' && sel1 != sel2){
         console.log('Iguales')
         
-        setInterval(() => { 
-            sel1.remove();
-            sel2.remove();
-    }, 1000)
+        let iguales = setInterval(() => { 
+            sel1.disabled = true;
+            sel2.disabled = true;
+            clearInterval(iguales)
+        }, 2000)
         resetValues();
-
-
-    } else if(sel1.textContent != sel2.textContent && sel2 != ''){
+        
+    } else if (sel1.id != sel2.id && sel2 != '' && sel1 != ''){
         console.log('Distintos')
+        let distinto = setInterval(() => {
+            sel1.style.visibility = 'hidden'
+            sel2.style.visibility = 'hidden'
+            clearInterval(distinto)
+        }, 1000)
+        
         resetValues();
+        
     } 
-
+    
 }
 
+let selectCard = (card) =>{
+    
+    if (card != null && card.disabled != true){
+        
+        showCard(card)
+        console.log(card)
+
+        if(sel1 == '' && sel2 == ''){
+            sel1 = card;
+        } else {
+            sel2 = card;
+            compare(sel1, sel2);
+        }
+    }
+}
 box.forEach(element => {
     element.addEventListener('click', (e) => {
-        console.log(e.target.firstChild)
+        /* console.log(e.target.firstChild) */
         
         selectCard(e.target.firstChild)
 })
 });
 
-//generando una lista de valores
 
-let list = []
-
-for(let i=0; i < cantcards; i++){
-
-    for (let j = 0; j < 2; j++) {
-            randomIndex = Math.round(Math.random() * cantcards)
-            list.splice(randomIndex, 0, i); //splice(ind, cant, elementos)
-        }
-
-    }
     
